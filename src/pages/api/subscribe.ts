b7232f103d0177b7290635d012b31ac76266d359
GabriellMatias <gabriel.goiasat@gmail.com>
2022-12-06 19:12:45 -0300

    :wrench: Feature: avoiding stripe user duplication with FaunDb Id

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -1,6 +1,17 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import { getSession } from 'next-auth/react'
 import { stripe } from '../../services/stripe'
+import { fauna } from '../../services/fauna'
+import { query as q } from 'faunadb'
+
+type User = {
+  ref: {
+    id: string
+  }
+  data: {
+    stripe_customer_id: string
+  }
+}
 
 export default async (req: NextApiRequest, response: NextApiResponse) => {
   /* verificando se e post pois quero criar algo dentro da API */
@@ -8,14 +19,37 @@ export default async (req: NextApiRequest, response: NextApiResponse) => {
     /* criar o usuario no STRIPE com as infos do banco de dados */
 
     const session = await getSession({ req })
-    const stripeCostumer = await stripe.customers.create({
-      email: session?.user?.email,
-      metadata: 'asd',
-    })
+
+    if (!session?.user?.email || !session?.user?.name) {
+      return response.status(400).json({
+        message: 'Logged user does not have an e-mail',
+      })
+    }
+
+    const user = await fauna.query<User>(
+      q.Get(
+        q.Match(q.Index('user_by_email'), q.Casefold(session?.user?.email)),
+      ),
+    )
+
+    let customerId = user.data.stripe_customer_id
+    if (!customerId) {
+      const stripeCostumer = await stripe.customers.create({
+        name: session?.user?.name,
+        email: session?.user?.email,
+      })
+      /* atualizando dados do usuario no faunaDB */
+      await fauna.query(
+        q.Update(q.Ref(q.Collection('users'), user.ref.id), {
+          data: { stripe_customer_id: stripeCostumer.id },
+        }),
+      )
+      customerId = stripeCostumer.id
+    }
 
     const checkoutSession = await stripe.checkout.sessions.create({
       /* pega o id do faunaDB do usuario e cria esse usuario no STRIPE pelo id */
-      customer: stripeCostumer.id,
+      customer: customerId,
 
       /* metodo de pagamento, se vai pedir endereco e quais items vao ser
       vendidos(pega o id preco do site da stripe) */
